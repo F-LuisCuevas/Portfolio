f76b0f58c033d38622a6b8b72bea5a4b29d15c3f
Luis Enrique Cuevas Picos <luiselectrico89@gmail.com>
2023-01-15 17:49:31 -0700

    changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,12 @@ import Intent from "./link"
 export default function Navbar() {
   return (
     <nav className="container my-3">
-      <ul className="nav justify-content-end">
-        <li className=" mx-4">
+        <div className=" mx-4">
           <Intent to="/">About</Intent>
-        </li>
-        <li className=" mx-4">
+        </div>
+        <div className=" mx-4">
           <Intent to="/projects">Projects</Intent>
-        </li>
-      </ul>
+        </div>
     </nav>
   )
 }
